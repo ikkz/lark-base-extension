3b3635b2c72243343a4303ef117a58f00df75ba5
ikkz <ylei.mk@gmail.com>
2024-07-02 08:03:21 +0000

    chore: move dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
   extends: ['@modern-js'],
-  ignorePatterns: ['/deploy'],
+  ignorePatterns: ['/dist'],
 };
--- apps/text-formatter/modern.config.ts
+++ apps/text-formatter/modern.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   output: {
     distPath: {
       html: '',
-      root: '../../deploy/text-formatter',
+      root: '../../dist',
     },
   },
   html: {
